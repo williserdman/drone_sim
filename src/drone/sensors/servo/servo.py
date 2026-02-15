53d7945a0f59833c5eb616649d8c878c9129957d
williserdman <willis.erdman@gmail.com>
2026-02-15 12:49:44 -0500

    angular servo

--- src/drone/sensors/servo/servo.py
+++ src/drone/sensors/servo/servo.py
@@ -1,14 +1,17 @@
-import RPi.GPIO as GPIO
-import time
+from gpiozero import AngularServo
+from time import sleep
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
-pwm = GPIO.PWM(17, 50)  # 50Hz
-pwm.start(0)
+# Initialize servo on GPIO 17, with specific pulse widths if needed
+# Common SG90 servos use default angles, but might need range adjustment
+servo = AngularServo(17, min_angle=0, max_angle=180)
 
-# Move to 90 degrees
-while True:
-    pwm.ChangeDutyCycle(7.5)
-    time.sleep(2)
-    pwm.ChangeDutyCycle(0)
-    time.sleep(2)
+try:
+    while True:
+        servo.angle = 0
+        sleep(1)
+        servo.angle = 90
+        sleep(1)
+        servo.angle = 180
+        sleep(1)
+except KeyboardInterrupt:
+    print("Program stopped")
