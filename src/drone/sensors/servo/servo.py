b8de2804aa27e48caef9835d37875b521dca9664
williserdman <willis.erdman@gmail.com>
2026-02-15 12:47:00 -0500

    using default api

--- src/drone/sensors/servo/servo.py
+++ src/drone/sensors/servo/servo.py
@@ -1,13 +1,14 @@
-from gpiozero import Servo
-from time import sleep
+import RPi.GPIO as GPIO
+import time
 
-# Connect the signal wire to GPIO pin 17
-servo = Servo(17)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+pwm = GPIO.PWM(17, 50)  # 50Hz
+pwm.start(0)
 
+# Move to 90 degrees
 while True:
-    servo.min()  # Move to -1 (minimum)
-    sleep(1)
-    servo.mid()  # Move to 0 (middle)
-    sleep(1)
-    servo.max()  # Move to 1 (maximum)
-    sleep(1)
+    pwm.ChangeDutyCycle(7.5)
+    time.sleep(2)
+    pwm.ChangeDutyCycle(0)
+    time.sleep(2)
