564b99a77848a33003ddfe2a5894638d2311307a
Donovan Crowley <donocrowley16@gmail.com>
2026-02-17 19:17:14 -0500

    servo

--- src/drone/sensors/servo/servo.py
+++ src/drone/sensors/servo/servo.py
@@ -3,7 +3,7 @@ from time import sleep
 
 # Initialize servo on GPIO 17, with specific pulse widths if needed
 # Common SG90 servos use default angles, but might need range adjustment
-servo = AngularServo(17, min_angle=0, max_angle=180)
+servo = AngularServo(18, min_angle=0, max_angle=180)
 
 try:
     while True:
