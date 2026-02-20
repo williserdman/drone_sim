d9b2fd0f3c8f6a89be865558f203bd15f7aadd38
Donovan Crowley <donocrowley16@gmail.com>
2026-02-19 20:12:41 -0500

    adding module typing

--- src/drone/waypoint_only.py
+++ src/drone/waypoint_only.py
@@ -2,13 +2,13 @@
 begins at point H, ascends to 10m and proceeds to waypoint A, proceeds to waypoint B, proceeds to waypoint C: descends
 """
 
-from common_types import *
-from control.mission_info import MissonTracker
-from control.drone_control import DroneControl
+from .common_types import *
+from .control.mission_info import MissonTracker
+from .control.drone_control import DroneControl
 import time
-from sensors.camera.camera import Camera
-from sensors.lidar.lidar import Lidar
-from utils.position_smoother import RelPosSmoother
+from .sensors.camera.camera import Camera
+from .sensors.lidar.lidar import Lidar
+from .utils.position_smoother import RelPosSmoother
 
 
 H = GPSCoord(101010, 101001, 100)
@@ -20,7 +20,7 @@ ALT_TOL = 0.4
 
 mt = MissonTracker()
 print("mission tracker initialized")
-controller = DroneControl(connection_port="/dev/ttyACM1")
+controller = DroneControl(connection_port="/dev/ttyACM0")
 print("controller init")
 camera = Camera(100)
 print("camera init")
