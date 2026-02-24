611a79db4d9b2c2ef02cb70c693692e55bc092bf
williserdman <willis.erdman@gmail.com>
2026-02-24 18:31:34 -0500

    dropper class

--- src/drone/sensors/servo/servo.py
+++ src/drone/sensors/servo/servo.py
@@ -1,6 +1,22 @@
 from gpiozero import Servo
 import time
 
+
+class Dropper:
+    def __init__(self, pins=[1, 7, 8, 25]):
+        self.servos = [
+            Servo(i, min_pulse_width=0.0005, max_pulse_width=0.0025) for i in pins
+        ]
+
+    def drop(self, delay_hold=3):
+        for s in self.servos:
+            s.max()
+        time.sleep(delay_hold)
+        for s in self.servos:
+            s.mid()
+
+
+""" 
 # Connect the servo signal wire to GPIO 17
 s1 = Servo(7, min_pulse_width=0.0005, max_pulse_width=0.0025)
 s2 = Servo(1, min_pulse_width=0.0005, max_pulse_width=0.0025)
@@ -23,4 +39,9 @@ try:
         time.sleep(5)
 
 except KeyboardInterrupt:
-    print("Program stopped")
+    print("Program stopped") """
+
+if __name__ == "__main__":
+    dropper = Dropper()
+    time.sleep(5)
+    dropper.drop()
