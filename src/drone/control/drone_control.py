f5f7c3019602f9ad416db22f0eb3a1f1ebe55d3b
williserdman <willis.erdman@gmail.com>
2026-03-27 16:04:16 -0400

    t

--- src/drone/control/drone_control.py
+++ src/drone/control/drone_control.py
@@ -281,7 +281,7 @@ class DroneControl:
         msg = self.vehicle.message_factory.landing_target_encode(
             0,  # time_usec (0 = use autopilot system time)
             0,  # target_num (0 = default target)
-            mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,  # coordinate frame
+            mavutil.mavlink.MAV_FRAME_BODY_NED,  # coordinate frame
             angle_x,  # X-axis angular offset
             angle_y,  # Y-axis angular offset
             target_distance,  # Scalar distance to target
