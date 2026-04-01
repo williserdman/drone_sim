3738af30b022c40bddf09aff4d4b90b16996542f
Anita Jiang <axj828@case.edu>
2026-03-31 23:56:33 -0500

    Update mission approval location from H to L

--- src/drone/missions/fm1.py
+++ src/drone/missions/fm1.py
@@ -2,7 +2,7 @@
 Mission:
 Begins at Home, takes off to 30ft AGL, transits horizontally to waypoint L, and lands.
 Must be carrying at least one payload to proceed to fm2.
-Wait flagger flag and judge approval at H before starting fm2.
+Wait flagger flag and judge approval at L before starting fm2.
 """
 
 from ..common_types import *
