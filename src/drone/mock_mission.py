be90eb3abc828272b8bf6d940180a39fe67abcec
williserdman <willis.erdman@gmail.com>
2026-04-02 12:44:33 -0400

    t

--- src/drone/mock_mission.py
+++ src/drone/mock_mission.py
@@ -103,8 +103,8 @@ def pickup_sequence(
         controller.goto_waypoint(target_wp)
 
         # Wait a moment for the drone to stabilize its tilt/roll after stopping
-        # controller.wait_until_stable()
-        controller.hold_waypoint_until_stable(target_wp)
+        controller.wait_until_stable()
+        # controller.hold_waypoint_until_stable(target_wp)
 
         # Search for target at this position
         for _ in range(5):
