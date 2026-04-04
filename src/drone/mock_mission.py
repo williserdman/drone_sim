e01f38630ec8ef51f3b8c98735c655ea5d640737
williserdman <willis.erdman@gmail.com>
2026-04-04 17:41:03 -0400

    t

--- src/drone/mock_mission.py
+++ src/drone/mock_mission.py
@@ -198,6 +198,7 @@ def fm3(
 
 if __name__ == "__main__":
     mt = MissonTracker(600)
+    mt.begin_mission()
     controller = DroneControl("/dev/ttyACM0")
     camera = Camera(50)
     lidar = Lidar()
