bbeba91d6d0b0883f197b9945eb535e447c09409
williserdman <willis.erdman@gmail.com>
2026-04-06 00:25:06 -0400

    video streaming backup

---
+++ src/backup/video_streamer.py
@@ -0,0 +1,27 @@
+import cv2
+from flask import Flask, Response
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)  # default webcam
+
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode(".jpg", frame)
+            frame = buffer.tobytes()
+            yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+
+
+@app.route("/video")
+def video_feed():
+    return Response(
+        generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=4321)  # i think i have wifi AP setup on 8080
