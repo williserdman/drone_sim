a464e22c1cd53e5c830525b438bb196ff6bc4781
williserdman <willis.erdman@gmail.com>
2026-04-08 19:46:00 -0400

    arming check

--- src/drone/mock_mission.py
+++ src/drone/mock_mission.py
@@ -59,7 +59,7 @@ def aruco_land_precision(
     # Loop until ArduPilot explicitly confirms touchdown
     # this for loop will exit after timeout -> 60 seconds
     for i in range(1_000):
-        if alt > ALT_TOL:
+        if alt > ALT_TOL and controller.vehicle.armed:
             if time.time() - t0 > timeout:
                 raise TimeoutError("Precision-landing timeout waiting for landed state")
             # while alt > ALT_TOL:
